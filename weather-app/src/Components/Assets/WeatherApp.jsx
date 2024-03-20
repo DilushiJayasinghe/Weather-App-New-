7b08395d5eaf5908a973e66357ff8d6c9021d512
DilushiJayasinghe <dilushijayasinghe02@gmail.com>
2024-03-20 20:42:32 +0530

    Add weather-API

---
+++ weather-app/src/Components/Assets/WeatherApp.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import './WeatherApp.css'
+import search_icon from '../Assets/search.png';
+import cloud_icon from '../Assets/cloud.png';
+import clear_icon from '../Assets/clear.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import humidity_icon from '../Assets/humidity.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+
+
+const WeatherApp = () => {
+
+    let api_key = "f658d43aebaf69430bbd11ab71b88cbf";
+
+return (
+    <div className='container'>
+        <div className='top-bar'>
+            <input type='text' className='cityInput' placeholder='Search'></input>
+            <div className='search-icon' onClick={() => { search() }}>
+                <img src={search_icon} alt="" />
+            </div>
+        </div>
+        <div className='weather-image'>
+            <img src={wicon} alt="" />
+        </div>
+
+        <div className="weather-temp">24°C</div>
+        <div className="weather-location">London</div>
+        <div className="data-container">
+            <div className="element">
+                <img src={humidity_icon} alt="" className="icon" />
+                <div className="data">
+                    <div className="humidity-percent">64%</div>
+                    <div className="text">Humidity</div>
+                </div>
+            </div>
+            <div className="element">
+                <img src={wind_icon} alt="" className='icon' />
+                <div className="data">
+                    <div className="wind-rate">18 km/h</div>
+                    <div className="text">Wind Speed</div>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default WeatherApp
